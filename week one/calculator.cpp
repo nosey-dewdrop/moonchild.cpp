b2980bd1c18c81b354a6c074e372d4f9c93a28c6
nosey-dewdrop <damummyphus@gmail.com>
2025-09-30 23:25:07 +0300

    ok,

--- week one/calculator.cpp
+++ week one/calculator.cpp
@@ -13,15 +13,51 @@ namespace damla {
 // in cpp the order is mattered. you cannot do something before defining it.
 // why include iostream? because we need to do input and output
 
-int add(int a, int b) {
+double add(int a, int b) {
             return a + b;
 };// cpp başka bir fonksiyonyu main içerisine yazamazsın.
 
+// fonksiyon sadece int parametre alabilir. double verirsen derleyici double'ı otomatik olarak inte dönüştütür.
+// type casting. bu veri kaybetmene neden olabilir.
+int subtract(int a, int b) {
+            return a - b;
+};// cpp başka bir fonksiyonyu main içerisine yazamazsın.
 
+int multiply(int a, int b) {
+    return a * b;
+};
+
+// here are the member funcitons which we defined inside the class.
+class Calculator{
+  public:
+    int add_member(int a, int b) {
+        return a + b;
+    }
+    int subtract_member(int a, int b) {
+        return a - b;
+    }
+    int multiply_member(int a, int b) {
+        return a * b;
+    }
+};
 
 int main(){
       damla::selam();
-      cout << "Welcome to the calculator app!" << endl;
-      cout << add(3,5) << endl;
+      cout<< "🦋today is 30.09.25. you are doing well girl boss. don't worry about anything, anyone."
+      << "it is over but it left a scar. it does not hurt you anymore. unless you let it." << endl;
+      cout << "welcome to the calculator app!" << endl;
+
+      int a, b;
+      cout << "enter a and b:"<< endl;
+      cin >> a >> b; 
+
+      Calculator c;
+      
+      // cout << "enter b: " << endl;
+      // cin >> b;    
+
+      cout << "a = " << a << "\nb = " << b << endl;
+      cout<< " (this is global function) a + b = " << add(a, b) << endl;
+      cout<< " (this is member function) a + b = " << c.add_member(a, b) << endl;
 }
 
